c07e80065833b1f6820a068da542afb1b8a9bb23
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2020-12-30 22:25:52 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ingredients from './Ingredients';
+
+const App = () => {
+  return (
+    <div>
+      <Ingredients />
+    </div>
+  );
+};
+
+export default App;
